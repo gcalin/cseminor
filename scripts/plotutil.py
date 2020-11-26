2edaebbc00df2b66c46a8cbddde79e333d57419a
calintheguy <gcalin224@gmail.com>
2020-11-26 08:11:22 +0100

    Adding average and error calculations and enhancing the functionality of plots.

--- scripts/plotutil.py
+++ scripts/plotutil.py
@@ -5,11 +5,68 @@ import matplotlib.pyplot as plt
 from scipy.stats import linregress
 import re
 
-def compute_averaged_values():
+def compute_averaged_values(filenames, cols, skip=0, stop=-1, column_major=False, separator='[\t ]'):
     """Computes the average values of multiple simulations.
-    """
-    pass
 
+    Args:
+        filenames ([string]): The names of the files to read.
+        cols ([int]): The columns to consider in each file.
+        skip (int, optional): How many lines to skip in the beginning (if any). Defaults to 0.
+        stop (int, optional): At which line to stop (if any). -1 means to read till the end.
+        column_major (bool, optional): Wehteher to return the the values per column (True) or per line (False). Defaults to False.
+        separator (str, optional): The string which separates values on a line. Defaults to '[\t ]'.
+
+    Returns:
+        [type]: A set of columns with the value of each entry equal to the average of the the entries in each file.
+    """
+
+    # Read columns
+    all_cols = [read_file_lines(file, cols, skip, stop, column_major, separator) for file in filenames]
+
+    # Initialise results array
+    res = [np.zeros(len(i)) for i in all_cols[0]]
+
+    # For each set of columns
+    for file_data in all_cols:
+        # For each column of the file
+        for num, col in enumerate(file_data):
+            # Add the value divided by the amount of files considered (to get the average)
+            res[num] += col/len(all_cols)
+
+    return res
+
+def compute_errors_per_column(filenames, cols, skip=0, stop=-1, column_major=True, separator='[\t ]'):
+    """Computes the error of the entries in a column across multiple files.
+
+    Args:
+        filenames ([string]): The names of the files to read.
+        cols ([int]): The columns to consider in each file.
+        skip (int, optional): How many lines to skip in the beginning (if any). Defaults to 0.
+        stop (int, optional): At which line to stop (if any). -1 means to read till the end.
+        column_major (bool, optional): Wehteher to return the the values per column (True) or per line (False). Defaults to False.
+        separator (str, optional): The string which separates values on a line. Defaults to '[\t ]'.
+
+    Returns:
+        [type]: A set of columns with the entries on each column calculated as 100*stddev/mean across that entry in multiple files.
+    """
+
+    # Read the files
+    all_cols = [read_file_lines(file, cols, skip, stop, column_major, separator) for file in filenames]
+
+    # Initialise results array
+    res = [[[] for j in all_cols[0][0]] for i in all_cols[0]]
+
+    # For each set of columns
+    for file_data in all_cols:
+        # For each column in the file
+        for i, col in enumerate(file_data):
+            # For each value in the column
+            for j, val in enumerate(col):
+                # Append the value to a tuple in the results array
+                res[i][j].append(val)
+
+    # Return the error at each time step across all columns
+    return [[np.std(valtuple)*100/np.average(valtuple) for valtuple in col] for col in res]
 
 def read_file_lines(filename, cols, skip=0, stop=-1, column_major=False, separator='[\t ]'):
     """Reads real values from the columns from a file.
@@ -20,7 +77,7 @@ def read_file_lines(filename, cols, skip=0, stop=-1, column_major=False, separat
         skip (int, optional): How many lines to skip in the beginning (if any). Defaults to 0.
         stop (int, optional): At which line to stop (if any). -1 means to read till the end.
         column_major (bool, optional): Wehteher to return the the values per column (True) or per line (False). Defaults to False.
-        separator (str, optional): The string which separates values on a column. Defaults to ' '.
+        separator (str, optional): The string which separates values on a line. Defaults to '[\t ]'.
 
     Returns:
         [[numpy.float64]]: A list of the read values.
@@ -39,7 +96,7 @@ def read_file_lines(filename, cols, skip=0, stop=-1, column_major=False, separat
     res = [[np.float64(line[col]) for col in cols] for line in [re.split(separator, l.strip()) for l in lines]]
     return np.transpose(res) if column_major else res
 
-def plot(title, xlabel, ylabel, grid, vals, labels, loglog=True, linear=None):
+def plot(title, xlabel, ylabel, grid, vals, labels, loglog=True, linear=None, show_slope=True):
     """Plots multiple sets of values on a common grid.
 
     Args:
@@ -51,28 +108,22 @@ def plot(title, xlabel, ylabel, grid, vals, labels, loglog=True, linear=None):
         labels ([string]): A list of names for each set of values.
         loglog (bool, optional): Whether or not to plot the values on a doubly logarithmic scale. Defaults to True.
         linear ([int], optional): A list of sets containing the starting and ending indices of the linear part of the graph.
+        show_slope (bool, optional): Whether or not to display the slope of the plot. Defaults to True.
         
     Returns:
-        slopes ([float], optinal): contains the slopes of the linear parts if lienar is not None.
+        slopes ([float], optinal): contains the slopes of the linear parts if linear is not None.
     """
 
     assert len(labels) == len(vals)
     if linear != None:
         assert len(labels) == len(linear)
     
-    # Set up plot
-    plt.figure(title)
-    plt.clf()
-    plt.title(title)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    
     slopes = []
 
     # Plot for each pair of values and labels
     for i in range(len(labels)):
-        value = vals[i];
-        label = labels[i];
+        value = vals[i]
+        label = labels[i]
 
         # Compute the slope for a particular set of values
         if linear == None:
@@ -86,19 +137,22 @@ def plot(title, xlabel, ylabel, grid, vals, labels, loglog=True, linear=None):
         # Generate a random (RGB) color. For common style, we should probably remove this in the future
         random_color = (random.random(), random.random(), random.random())
 
+        # Set up plot
+        plt.clf()
+        plt.title(title)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+
         # Plot the values and the slope
         if loglog:
             plt.loglog(grid, value, c=random_color, label=label, marker='o')
-            plt.loglog(grid, slope*grid, '--', c=random_color, label = "slope of " + label)
+            if show_slope:
+                plt.loglog(grid, slope*grid, '--', c=random_color, label = "slope of " + label)
             
         else:
-<<<<<<< HEAD
-            plt.plot(grid, value, c= random_color, label=label, marker='o')
-            plt.plot(grid, slope*grid, '--', c=random_color, label = "slope of " + label)
-=======
             plt.plot(grid, value, c=random_color, label=label, marker='o')
-            plt.plot(grid, slope*grid, '--', label = "slope of " + label)
->>>>>>> 3b53356accedd64c07bd11f5ecdfe23c4a9d81f7
+            if show_slope:
+                plt.plot(grid, slope*grid, '--', c=random_color, label = "slope of " + label)
 
         plt.legend()        
         plt.show()
@@ -108,19 +162,44 @@ def plot(title, xlabel, ylabel, grid, vals, labels, loglog=True, linear=None):
     else:
         return None
 
+# TODO: remove global variable in the future
+paths = ['./../data/H2O/run1/', './../data/H2O/run2/', './../data/H2O/run3/']
+
 def plot_diffusivity():
-    lines = read_file_lines('./../data/H2O/selfdiffusivity.dat', [0, 1, 2], skip=3, column_major=True)
+
+    # Particular filenames for diffusivity
+    filenames = [path + 'selfdiffusivity.dat' for path in paths]
+    #lines = read_file_lines('./../data/H2O/run1/selfdiffusivity.dat', [0, 1, 2], skip=3, column_major=True)
+
+    # Read the lines and plot the results
+    lines = compute_averaged_values(filenames, [0,1,2], skip=3, column_major=True)
     self_diff = plot('Plot of diffusivity', 'Time', r'$MSD_{Diffusivity}$', lines[0], lines[1:], ['Hydrogen', 'Oxygen'], linear=[[15,len(lines[0])], [11,len(lines[0])]])
+    
+    # Get the array of errors at each point and plot
+    err = compute_errors_per_column(filenames, [1,2], skip=3, column_major=True)
+    plot('Errors in diffusivity', 'Time', r'100*stddev/mean', np.log10(lines[0]), err, ['Hydrogen', 'Oxygen'], loglog=False, linear=None, show_slope=False)
     return self_diff
 
 def plot_viscosity():
-    lines = read_file_lines('./../data/H2O/viscosity.dat', [0, 8, 9], skip=3, column_major=True)
+
+    # Particular filenames for viscosity
+    filenames = [path + 'viscosity.dat' for path in paths]
+    #lines = read_file_lines('./../data/H2O/run1/viscosity.dat', [0, 8, 9], skip=3, column_major=True)   
+
+    # Read the lines and plot the results
+    lines = compute_averaged_values(filenames, [0,8,9], skip=3, column_major=True)
     visc = plot('Plot of viscosity', 'Time', r'$MSD_{Viscosity}$', lines[0], lines[1:], ['MSD_all', 'MSD_bulkvisc'], linear=[[23,39], [24,36]])
+
+    # Get the array of errors at each point and plot
+    err = compute_errors_per_column(filenames, [8,9], skip=3, column_major=True)
+    plot('Errors in viscosity', 'Time', r'100*stddev/mean', np.log10(lines[0]), err, ['MSD_all', 'MSD_bulkvisc'], loglog=False, linear=None, show_slope=False)
     return visc
 
 def plot_rdf():
-    lines = read_file_lines('./../data/H2O/rdf.dat', [0, 2, 4, 6], skip=1, column_major=True)
-    plot('Plot of radial distribution function (rdf)', 'Radius', r'Density', lines[0], lines[1:], ['Hydrogen-Hydrogen', 'Hydrogen-Oxygen', 'Oxygen-Oxygen'])
+    filenames = [path + 'rdf.dat' for path in paths]
+    #lines = read_file_lines('./../data/H2O/run1/rdf.dat', [0, 2, 4, 6], skip=1, column_major=True)
+    lines = compute_averaged_values(filenames, [0, 2, 4, 6], skip=1, column_major=True)
+    plot('Plot of radial distribution function (rdf)', 'Radius', r'Density', lines[0], lines[1:], ['Hydrogen-Hydrogen', 'Hydrogen-Oxygen', 'Oxygen-Oxygen'], show_slope=False)
     
 N=800 #number of molecules
 self_diff = plot_diffusivity()
